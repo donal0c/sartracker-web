3d5ecd7ce415c39824a3406e6a1a79d38096f846
Donal <ocallaghandonal2@gmail.com>
2026-09-03 21:36:44 +0100

    chore(build): bind package to cleanup remediation head

--- src/lib/version.generated.ts
+++ src/lib/version.generated.ts
@@ -1,2 +1,2 @@
 export const APP_VERSION_BASE = "0.1.0-beta.12.11"
-export const APP_VERSION_BUILD_ID = "sha.679ba8fd2923"
+export const APP_VERSION_BUILD_ID = "sha.ac8bdcfffbd81a696f23ddbaf389218d54428121"
